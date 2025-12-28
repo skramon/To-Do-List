03d4e1b2f9df403ae71ee9b625c9793ddf82c590
Ramon Gomes <ramonsousaa09@gmail.com>
2025-12-28 00:12:33 -0300

    clicks
    adicionei o X para fechar a aba de adicionar tarefas, fecha no X e quando clica no overlay

---
+++ index.js
@@ -0,0 +1,9 @@
+function novaTarefa(){
+    overlay.classList.add('active');
+    criarTarefa.classList.add('active');
+}
+
+function fecharModal(){
+    overlay.classList.remove('active');
+    criarTarefa.classList.remove('active');
+}
