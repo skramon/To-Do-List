2662d3cae402226f54e547a0bae8836fa9b43a89
Ramon Gomes <ramonsousaa09@gmail.com>
2025-12-28 21:07:04 -0300

    adicionando fuções
    adicionando funções no java script

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ function fecharModal(){
     overlay.classList.remove('active');
     criarTarefa.classList.remove('active');
 }
+
+function buscarTarefas(){
+    fetch('http://localhost:3000/tarefas')
+    .then(res => res.json())
+    .then(res => res.json())
+    .then(res => {
+    })
+} buscarTarefas();
+
+function adicionarTarefa(listaDeTarefas){
+    if(listaDeTarefas.length > 0){
+        listaDeTarefas.map(tarefa => {
+            Lista.innerHTML += `
+            <li>
+                <h5>${tarefa.titulo}</h5>
+                <p> ${tarefa.descricao}</p>
+                <div class="actions">
+                    <box-icon name='trash' size='smcs'></box-icon>
+                </div>
+            </li>
+            `;
+        })
+    }
+}
