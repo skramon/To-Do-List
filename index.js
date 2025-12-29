ef47fd16ea3ff0c4e2eaf650176a467af9a58d7f
Ramon Gomes <ramonsousaa09@gmail.com>
2025-12-29 20:57:46 -0300

    erro

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function abriModal(){
-    overlay.classList.add('active');
-    criarTarefa.classList.add('active');
-}
-
-function fecharModal(){
-    overlay.classList.remove('active');
-    criarTarefa.classList.remove('active');
-}
-
 function buscarTarefas(){
     Lista.innerHTML = '';
     fetch('http://localhost:3000/tarefas')
@@ -56,50 +46,3 @@ function novaTarefa(){
         titulo: titulo.value,
         descricao: descricao.value,
     };
-
-   fetch("http://localhost:3000/tarefas",
-   {
-       method: "POST",
-       headers: {
-           "Content-Type": "application/json"
-       },
-       body: JSON.stringify(tarefa)
-   })
-   .then(res => res.json())
-   .then(res => {
-       console.log(res);
-       fecharModal();
-       buscarTarefas();
-       let fomr = document.querySelector("#criarTarefa form");
-       fomr.reset();
-   })
-}
-
-function deletarTarefa(id){
-    fetch(`http://localhost:3000/tarefas/${id}`,
-    {
-        method: "DELETE",
-    })
-    .then(res => res.json())
-    .then(res => {
-        buscarTarefas();
-    })
-}
-
-function pesquisarTarefa(){
-    let lis = document.querySelectorAll("ul li");
-    if(busca.value.length > 0){
-        lis.forEach(li => {
-            if (!li.children[0].innerText.includes(busca.value)){
-                li.classList.add("oculto");
-            }else{
-                li.classList.remove("oculto");
-            }
-        })
-    } else {
-        lis.forEach(li => {
-            li.classList.remove("oculto");
-        })
-    }
-
-}
